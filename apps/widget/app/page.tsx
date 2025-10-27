b37fbba9dba1e0dc6746d5e5b08f0ea2b36658d1
wy2010344 <wangyang2010344@foxmail.com>
2025-10-27 11:53:56 +0800

    add voice assistant

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,17 +1,28 @@
 'use client'
 import { Button } from '@workspace/ui/components/button'
 
-import { useQuery } from 'convex/react'
-import { api } from '@workspace/backend/convex/_generated/api'
+import { useVapi } from '@/modules/widget/hooks/use-vapi'
 
 export default function Page() {
-  const users = useQuery(api.users.getMany)
+  const {
+    isConnected,
+    isConnecting,
+    isSpeaking,
+    transcript,
+    startCall,
+    endCall,
+  } = useVapi()
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        {JSON.stringify(users, null, 2)}
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <Button onClick={() => startCall()}>Start call</Button>
+        <Button onClick={() => endCall()} variant="destructive">
+          End call
+        </Button>
+        <p>isConnected: {isConnected + ''}</p>
+        <p>isConnecting: {isConnecting + ''}</p>{' '}
+        <p>isSpeaking: {isSpeaking + ''}</p>
+        <pre>{JSON.stringify(transcript, null, 2)}</pre>
       </div>
     </div>
   )
