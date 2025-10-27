5d07f8a0c1d6d2ad9b3819d697f365f4a1d5bc36
wy2010344 <wangyang2010344@foxmail.com>
2025-10-27 15:02:38 +0800

    feat: dashboard layout

--- apps/web/app/(dashboard)/layout.tsx
+++ apps/web/app/(dashboard)/layout.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { AuthGuard } from '../auth/ui/components/auth-guard'
+import { DashboardLayout } from '@/module/dashboard/ui/layouts/dashboard-layout'
+import { cookies } from 'next/headers'
 
 const Component: React.FC<{
   children?: React.ReactNode
 }> = function ({ children }) {
-  return <AuthGuard>{children}</AuthGuard>
+  return <DashboardLayout>{children}</DashboardLayout>
 }
 
 export default Component
---
+++ apps/web/app/(dashboard)/plugins/vapi/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Component: React.FC<{}> = function () {
+  return <div>vapi</div>
+}
+
+export default Component
